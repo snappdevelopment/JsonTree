454c7f1d24acd6ce51a8d60c3e15ec7b3b0d5780
snappdevelopment <se.neu@web.de>
2024-02-20 07:28:52 +0100

    small adjustments

--- jsontree/src/main/java/com/sebastianneubauer/jsontree/JsonTreeParserState.kt
+++ jsontree/src/main/java/com/sebastianneubauer/jsontree/JsonTreeParserState.kt
@@ -3,7 +3,7 @@ package com.sebastianneubauer.jsontree
 import kotlinx.serialization.json.JsonElement
 
 internal sealed interface JsonTreeParserState {
-    object Loading : JsonTreeParserState
+    data object Loading : JsonTreeParserState
     data class Ready(val list: List<JsonTreeElement>) : JsonTreeParserState
 
     sealed interface Parsing : JsonTreeParserState {
